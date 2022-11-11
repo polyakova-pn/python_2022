351fff2b3f46c9ebe2056fcfe78a09c5809192c0
Иван Маслаков <ivamaslakov@gmail.com>
2022-11-11 16:03:56 +0300

    14 commit

--- classwork09/sharik_game.py
+++ classwork09/sharik_game.py
@@ -1,46 +1,9 @@
-def game(screen, COLORS, FPS_local, FPS_total):
-    '''
-    Основная функция, запускает процесс игры, считает попадания, вырождает новые порции объектов
-    в рандомные моменты выводит на экран очень дорогие, но очень маленькие белые точки
-    :param screen: экран на котором все происходит
-    :param COLORS: список цветов объектов
-    :param FPS_local: тики, с которыми перемещается данная партия шаров
-    :param FPS_total: тики, с которыми вырождаются новые порции объектов
-    '''
-    pygame.display.update()
-    clock = pygame.time.Clock()
-    finished = False
-
-    score = 0
-    while not finished:
-        current_balls = new_ball(COLORS)
-        dot_exist = False
-        if randint(1, 20) == 1:
-            dot_exist = True
-            current_dot = dot()
-        for i in range(int(FPS_local // FPS_total)):
-            clock.tick(FPS_local)
-            current_balls = motion(current_balls)
-            if dot_exist:
-                make_dot(current_dot)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    finished = True
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    print('Click!')
-                    if popal(event, current_balls):
-                        score += 1
-                        print('ПОПАЛ! счет:', score)
-                    if popal_dot(event, current_dot):
-                        score += 10
-                        print('ПОПАЛ В ТОЧКУ! счет:', score)
-            pygame.display.update()
-
-        pygame.display.update()
-        screen.fill((0, 0, 0))
+import pygame
+from pygame.draw import circle
+from random import randint
 
 
-def make_dot(current_dot):
+def make_dot(screen, current_dot):
     '''
     создает точку в данном месте
     :param current_dot: параметры точки (координаты и радиус)
@@ -50,36 +13,36 @@ def make_dot(current_dot):
     y = current_dot[1]
     r = current_dot[2]
     circle(screen, (255, 255, 255), (x, y), r)
-    return
-def dot():
+
+
+def dot(screen):
     '''
     создает точку
     :return: координаты и радиус точки
     '''
     x = randint(101, 1099)
     y = randint(101, 799)
-    r = 3
+    r = 10
     circle(screen, (255, 255, 255), (x, y), r)
     current_dot = [x, y, r]
     return current_dot
+
 
 def popal_dot(event, current_dot):
     '''
     определяет местоположение клика и показывает, попал ли игрок по шару
     mx, mу - координаты мыши при нажатии
     x, y - координаты точки при нажатии
+    current_dot - массив содержащий координаты и радиус точки
     dest - расстояние от цетнра точки до курсора при нажатия
     popadanie - переменная попадания или промаха
 
     '''
     mx = event.pos[0]
     my = event.pos[1]
-    popadanie_hot_v_odin = False
-
-    x = current_dot[0]
-    y = current_dot[1]
-    r = current_dot[2]
+    x, y, r = current_dot
     dest = ((mx - x) ** 2 + (my - y) ** 2) ** 0.5
+    popadanie = False
     if dest <= r:
         popadanie = True
     return popadanie
@@ -97,29 +60,28 @@ def popal(event, current_balls):
     my = event.pos[1]
     popadanie_hot_v_odin = False
     for ball in current_balls:
-        x = ball[0]
-        y = ball[1]
-        r = ball[2]
+        x, y, r = ball[0:3]
         dest = ((mx - x) ** 2 + (my - y) ** 2) ** 0.5
         if dest <= r:
             popadanie_hot_v_odin = True
+            current_balls.remove(ball)
     return popadanie_hot_v_odin
 
 
 def make_colors():
     '''создает список цветов'''
-    RED = (255, 0, 0)
-    BLUE = (0, 0, 255)
-    YELLOW = (255, 255, 0)
-    GREEN = (0, 255, 0)
-    MAGENTA = (255, 0, 255)
-    CYAN = (0, 255, 255)
-    return [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+    red = (255, 0, 0)
+    blue = (0, 0, 255)
+    yelow = (255, 255, 0)
+    green = (0, 255, 0)
+    magenta = (255, 0, 255)
+    cyan = (0, 255, 255)
+    return [red, blue, yelow, green, magenta, cyan]
 
 
-def new_ball(COLORS):
+def new_ball(screen, colors):
     '''
-    COLORS - список возможных цветов
+    colors - список возможных цветов
     n - количество шаров в данной партии
 
     рисует новую партию шариков с рандомными параметрами
@@ -137,14 +99,14 @@ def new_ball(COLORS):
         x = randint(101, 1099)
         y = randint(101, 799)
         r = randint(10, 100)
-        speed_x = randint(-20, 20)
-        speed_y = randint(-20, 20)
-        color = COLORS[randint(0, 5)]
+        speed_x = randint(-1 * r // 3, r // 3)
+        speed_y = randint(-1 * r // 3, r // 3)
+        color = colors[randint(0, 5)]
         circle(screen, color, (x, y), r)
         current_balls.append([x, y, r, color, speed_x, speed_y])
     return current_balls
 
-def motion(current_balls):
+def motion(screen, current_balls, lenth, wight):
     '''
     в соответствии с параметром скорости speed сдвигает каждый шарик в данной партии и возвращает
     текущие состояние системы, если шарик близок к стенке, меняет направление его скорости
@@ -166,25 +128,71 @@ def motion(current_balls):
         circle(screen, color, (x + speed_x, y + speed_y), r)
         i[0] = x + speed_x
         i[1] = y + speed_y
-        if x > 1200 - r or x < r:
+        if x > lenth - r or x < r:
             i[4] = -1 * i[4]
-            i[0] = x - 2 * speed_x
-        if y > 900 - r or y < r:
+            i[0] = x - speed_x
+        if y > wight - r or y < r:
             i[5] = -1 * i[5]
-            i[1] = y - 2 * speed_y
+            i[1] = y - speed_y
     return current_balls
 
 
-import pygame
-from pygame.draw import *
-from random import randint
+def game(colors, fps_local, fps_total, lenth, wight):
+    """
+    Основная функция, запускает процесс игры, считает попадания, вырождает новые порции объектов
+    в рандомные моменты выводит на экран очень дорогие, но очень маленькие белые точки
+    :param screen: экран на котором все происходит
+    :param colors: список цветов объектов
+    :param fps_local: тики, с которыми перемещается данная партия шаров
+    :param fps_total: тики, с которыми вырождаются новые порции объектов
+    """
+    screen = scr(lenth, wight)
+    pygame.display.update()
+    clock = pygame.time.Clock()
+    finished = False
+
+    score = 0
+    while not finished:
+        current_balls = new_ball(screen, colors)
+        dot_exist = False
+        if randint(1, 6) == 1:
+            dot_exist = True
+            current_dot = dot(screen)
+        for i in range(int(fps_local // fps_total)):
+            clock.tick(fps_local)
+            current_balls = motion(screen, current_balls, lenth, wight)
+            if dot_exist:
+                make_dot(screen, current_dot)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finished = True
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print('Click!')
+                    if popal(event, current_balls):
+                        score += 1
+                        print('ПОПАЛ! счет:', score)
+                    elif dot_exist and popal_dot(event, current_dot):
+                        score += 10
+                        print('ПОПАЛ В ТОЧКУ! счет:', score)
+                    else:
+                        print('MIMO')
+            pygame.display.update()
+
+        pygame.display.update()
+        screen.fill((0, 0, 0))
+
+
+def scr(lenth, width):
+    screen = pygame.display.set_mode((lenth, width))
+    return screen
 pygame.init()
 
-FPS_total = 0.1
-FPS_local = 10
-screen = pygame.display.set_mode((1200, 900))
-COLORS = make_colors()
+fps_total = 0.2
+fps_local = 40
+lenth = 1200
+wight = 800
+colors = make_colors()
 
-game(screen, COLORS, FPS_local, FPS_total)
+game(colors, fps_local, fps_total, lenth, wight)
 
 pygame.quit()
